80dcfbc056af39067b2efbacce1cb174f5e598a1
UAhmetova <uliaahm.com@yandex.ru>
2026-02-13 21:12:52 +0300

    Добавление эндпоинтов в контроллер для фильмов

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,49 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @PostMapping
+    public Film createFilm(@RequestBody Film film) {
+        film.setId(getNextId());
+
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film newFilm) {
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+
+            oldFilm.setDescription(newFilm.getDescription());
+            return oldFilm;
+        }
+
+        return null;
+    }
+
+    @GetMapping
+    public Collection<Film> findAllFilms() {
+        return films.values();
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
